62689239516639e8957929ff1fa175ffa9ca35ce
ImNotReall <yixiangsps@gmail.com>
2017-01-20 04:54:34 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+void showMenu(void);
+void dispTotal(void);
+void dispQuarter(void);
+void dispTop(void);
+void dispMin(void);
+void dispHousehold(void);
+void dispDiff(void);
+
+void main(void)
+{
+	char userinput;
+	do {
+		showMenu();
+		scanf(" %c", &userinput);
+		fflush(stdin);
+		switch (userinput)
+		{
+		case 'A': case 'a':
+			dispTotal();
+			break;
+		case 'B': case'b':
+			dispQuarter();
+			break;
+		case 'C': case'c':
+			dispTop();
+			break;
+		case 'D': case'd':
+			dispMin();
+			break;
+		case 'E': case'e':
+			dispHousehold();
+			break;
+		case 'F': case'f':
+			dispDiff();
+			break;
+		case 'Q': case 'q':
+			userinput = 'Q';
+			printf("============================\n");
+			puts("You have exited the program.");
+			printf("============================\n");
+			break;
+		default:
+			printf("=============================================================\n");
+			printf("You have entered an invalid selection. Please try again.\n");
+			printf("=============================================================\n");
+		}
+	} while (userinput != 'Q');
+}
